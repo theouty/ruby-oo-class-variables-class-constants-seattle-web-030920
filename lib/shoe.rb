4848d47e58e38c5fa379e39ae132fe8b15eda166
Aman Michael <aman.david.michael@gmail.com>
2020-03-12 17:29:27 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -9,8 +9,11 @@ class Shoe
 
   def brands=(brand)
     @brand = brand
+    if BRANDS.include?(brand) == false
     BRANDS << brand
+    end
   end
+  
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
